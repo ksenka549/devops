7e0a8afc6a9ed44af08e43e96f73fd650a5f820c
ksenka <ugrynks@gmail.com>
2022-03-25 10:18:39 +0200

    add change owner script

---
+++ bash/changeowner.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Set variables
+user=$1
+mydir=$2
+
+# Stage 1: Check premissions
+if [ "$EUID" -ne 0 ] ;
+  then 
+   echo "$(tput setaf 1)[Error:] $(tput setaf 7)Please run as root"
+  exit
+fi
+
+# Stage 2: Check parameters count
+if [ $# != 2 ] ; 
+  then
+   echo "$(tput setaf 1)[Error:] $(tput setaf 7)Illegal number of parameters - $#. Please add only 2 parameters" >&2
+  exit
+fi
+
+# Stage 3: Check the user exist
+if ! id -u "$user" >/dev/null 2>&1 
+  then
+   echo "$(tput setaf 1)[Error:] $(tput setaf 7)The user '$user' does not exists."
+  exit
+fi
+
+# Stage 4: Check the directory exist
+if [ ! -d "$mydir" ] ; then 
+   echo "$(tput setaf 1)[Error:] $(tput setaf 7) The directory $mydir does not exists."
+  exit
+fi
+
+# Stage 5: Change owner
+  chown -R "$user" "$mydir"
+  echo "$(tput setaf 2)[Sucsess:] $(tput setaf 7)The owner of the $(tput setaf 5)$mydir $(tput setaf 7)has been changed to user $(tput setaf 3)$user"
+
